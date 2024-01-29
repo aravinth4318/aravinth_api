2a33b8aef4543fd307b1e352851371539c6fdda4
Manoj2k15 <cmanojkumar386@gmail.com>
2024-01-29 12:14:12 +0530

    apicatfact

---
+++ lib/ara_api1.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'ara_model_class/api_catfact.dart';
+import 'package:http/http.dart' as http;
+
+class ap_catfact extends StatefulWidget {
+  const ap_catfact({super.key});
+
+  @override
+  State<ap_catfact> createState() => _ap_catfactState();
+}
+
+class _ap_catfactState extends State<ap_catfact> {
+  Future<api_catfact> cat_factDetails() async {
+    var catfact = await http.get(Uri.parse("https://catfact.ninja/fact"));
+    return api_catfact.fromJson(jsonDecode(catfact.body));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        FutureBuilder(
+          future: cat_factDetails(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Text(snapshot.data!.fact.toString()),
+                  Text(snapshot.data!.length.toString())
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ]),
+    );
+  }
+}
