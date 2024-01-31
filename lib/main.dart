3135dfbdca43c5f8e68d2676a9b2d24eae627ad1
Manoj2k15 <cmanojkumar386@gmail.com>
2024-01-31 17:26:36 +0530

    apicatfact55

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: nati1 (),
+      home: api_bottomnavi (),
     );
   }
 }
